7c936a048957b7ba701581144167c14683876b5c
patrikcerbak <115928463+patrikcerbak@users.noreply.github.com>
2025-10-15 14:27:05 +0200

    Fixed bugs, added missing remappings and more tests (#10)
    * moved the "if (SanitizeGC)" from remapAddress methods to the mapper itself
    
    * renamed the ".*remap.*Address" methods to the "map.*AddrTo.*Addr" format
    
    * removed unnecessary using statements
    
    * added basic test with multithreaded allocation
    
    * added address remapping before pointer_delta is called
    
    * reverted assert skipping for SanitizeGC
    
    * added missing address remappings
    
    * added more logging and asserts when moving regions
    
    * added more tests to the suite testing gc and "everyday" situations

--- src/hotspot/share/gc/g1/sanitizeAddressMapper.hpp
+++ src/hotspot/share/gc/g1/sanitizeAddressMapper.hpp
@@ -1,39 +1,25 @@
 #ifndef SHARE_GC_G1_SANITIZEADDRESSMAPPER_HPP
 #define SHARE_GC_G1_SANITIZEADDRESSMAPPER_HPP
 
-#include "runtime/globals.hpp"
-
 class SanitizeGCMapper {
-private:
-    using byte = unsigned char;
-    using byte_ptr = const byte*;
-
 public:
 
-    static const void* mapNewAddrToOriginalAddr(const void* newAddr);
-    static const void* mapNewEdgeAddrToOriginalAddr(const void* newAddr);
-    static const void* mapOriginalAddrToNewAddr(const void *newAddr);
-    static const void* mapOriginalEdgeAddrToNewAddr(const void* newAddr);
+    static const void* mapNewAddrToOriginalAddrImpl(const void* newAddr);
+    static const void* mapNewEdgeAddrToOriginalAddrImpl(const void* newAddr);
+    static const void* mapOriginalAddrToNewAddrImpl(const void *newAddr);
+    static const void* mapOriginalEdgeAddrToNewAddrImpl(const void* newAddr);
 
-    template <typename T> static inline void remapAddress(T &addr) {
-      if (SanitizeGC) {
-        addr = (T) mapNewAddrToOriginalAddr(addr);
-      }
+    template <typename T> static inline void mapNewAddrToOriginalAddr(T &addr) {
+      addr = (T) mapNewAddrToOriginalAddrImpl(addr);
     }
-    template <typename T> static inline void remapEdgeAddress(T &addr) {
-        if (SanitizeGC) {
-            addr = (T) mapNewEdgeAddrToOriginalAddr(addr);
-        }
+    template <typename T> static inline void mapNewEdgeAddrToOriginalAddr(T &addr) {
+      addr = (T) mapNewEdgeAddrToOriginalAddrImpl(addr);
     }
-    template <typename T> static inline void reverseRemapAddress(T &addr) {
-        if (SanitizeGC) {
-            addr = (T) mapOriginalAddrToNewAddr(addr);
-        }
+    template <typename T> static inline void mapOriginalAddrToNewAddr(T &addr) {
+      addr = (T) mapOriginalAddrToNewAddrImpl(addr);
     }
-    template <typename T> static inline void reverseRemapEdgeAddress(T &addr) {
-        if (SanitizeGC) {
-            addr = (T) mapOriginalEdgeAddrToNewAddr(addr);
-        }
+    template <typename T> static inline void mapOriginalEdgeAddrToNewAddr(T &addr) {
+      addr = (T) mapOriginalEdgeAddrToNewAddrImpl(addr);
     }
 };
 
